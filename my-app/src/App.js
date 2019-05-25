3884aa5726b7d2e13dc161dbdff3d0f6af467670
YferHurtado <yennifer.hurtadoarce@gmail.com>
2019-05-25 12:32:23 -0500

    estructura de archivos, colocando parámetro al componente location

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import WeatherLocation from './components/WeatherLocation'
 import './App.css';
+
+// arrow function de una linea:no colocamos return
+// const miFunction = () => (material.length + 1);
+// en casa de más lineas llaves y el return
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <WeatherLocation />
       </header>
     </div>
   );
---
+++ my-app/src/components/Location.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+//Que el valor que le damos no sea fijo sino que responda 
+//segun el parametro que le pasemos
+//si una funcion es de una linea puedo hacerlo con parantesis
+//pero si es mas de una linea uso las llaves con el return
+
+const Location = (props) => {
+    console.log(props)
+    debugger;
+    return (
+    <div>
+        <h1></h1>
+    </div>
+    )
+};
+
+export default Location;
